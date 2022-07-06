de08db09a6edc0f4af05bae3206a718096a33c6c
junliangl <junliangl@moqi.ai>
2022-07-06 17:34:06 +0800

    test

--- TestRunner/Runner.py
+++ TestRunner/Runner.py
@@ -39,13 +39,13 @@ if __name__ == '__main__':
         url='https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=020eb2a4-78a4-47f2-90c3-fb97da4f7f4c',
         headers={'Content-Type': 'application/json;charset=UTF-8'},
         json={
-            "msgtype": "text",
-            "text": {
+            "msgtype": "markdown",
+            "markdown": {
                 "content": f"成功的用例数: {test_count.get_count()[0]}\n"
                            f"失败的用例数: {test_count.get_count()[1]}\n"
                            f"错误的用例数：{test_count.get_count()[2]}\n"
                            f"测试的用例总数：{test_count.get_count()[0] + test_count.get_count()[1] + test_count.get_count()[2]}\n"
-                           f"测试报告地址: 10.1.1.156/ui-auto-test/{now_time}-test.html"
+                           f"测试报告地址: [详情地址](10.1.1.156/ui-auto-test/{now_time}-test.html)"
             }
         }
     )
